2b02ee041c7722c75a251003e27e20c415d70c53
Hannes <hannes.study@gmail.com>
2023-12-05 21:00:24 +0200

    added simple tasks of changeing images.

--- src/main/java/desk/app/HelloController.java
+++ src/main/java/desk/app/HelloController.java
@@ -8,21 +8,32 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
+@FXML
+    ImageView moogleView;
+@FXML
+    Button switchMoogle;
+    Image moogleImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("moogle11.png")));
 
     public Button login_button;
     private Stage stage;
     private Scene scene;
-
     private Parent root;
+    private Integer counter;
 
 @FXML
     private TextField username_text_field;
 
+    public void displayMoogle(){
+        moogleView.setImage(moogleImage);
+    }
 
     public void  login(ActionEvent event)throws IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("scene2.fxml"));
