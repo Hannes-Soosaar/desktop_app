86384f2d898e59c3fa1e6692a92c694424e9a672
Hannes <hannes.study@gmail.com>
2023-12-05 15:04:31 +0200

    Added an alert and logout button.

--- src/main/java/desk/app/HelloApplication.java
+++ src/main/java/desk/app/HelloApplication.java
@@ -1,27 +1,51 @@
 package desk.app;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @FXML
-    Label userLabel;
+    private Parent root;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        String css =this.getClass().getResource("application.css").toExternalForm();
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        String css = this.getClass().getResource("application.css").toExternalForm();
         scene.getStylesheets().add(css);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            logout(stage);
+        });
     }
+
+    public void logout(Stage stage) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("Your about to log out of the hello app");
+        alert.setContentText("Do you want to save?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            System.out.println("You logged out");
+            stage.close();
+        }
+    }
+
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/desk/app/Scene2Controller.java
+++ src/main/java/desk/app/Scene2Controller.java
@@ -1,14 +1,40 @@
 package desk.app;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Scene2Controller {
     @FXML
     private Label nameLabel;
+    @FXML
+    private AnchorPane userPane;
+    @FXML
+    private Button logoutButton;
 
-     public void displayName(String userName){
-       nameLabel.setText("Hey:"+ userName);
-     }
+    Stage stage;
+
+    public void logout(ActionEvent event) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("Your about to log out of the hello app");
+        alert.setContentText("Do you want to save?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) userPane.getScene().getWindow();
+            System.out.println("You logged out");
+            stage.close();
+        }
+    }
+
+    public void displayName(String userName) {
+        nameLabel.setText("Hey:" + userName);
+    }
 
 }
